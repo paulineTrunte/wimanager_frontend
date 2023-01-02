8c8efa83dee7fbe1ac3654a263c37575edc5c170
paulineTrunte <pauline.trunte@student.htw-berlin.de>
2023-01-02 14:54:36 +0100

    Login created

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 
 
 <template>
-  <div id="nav">
+  <div>
+    <nav class="green">
+      <div class="nav-wrapper">
+        <span class="brand logo">Wirtschaftsinformatik Manager</span>
+        <ul class="right">
+          <li><a @click.prevent="login" v-if="!activeUser">Login</a></li>
+          <li><a @click.prevent="logout" v-if="activeUser">Logout</a></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/modules">Module</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view/> -->
 </template>
 
 <script>
 
-import Modul from './views/ModuleView.vue'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      activeUser: null
+    }
+  },
+  async created () {
+   await this.refreshActiveUser()
+  },
+  watch: {
+   '$route': 'refreshActiveUser'
+  },
+  methods: {
+    login () {
+        this.$auth.loginRedirect()
+    },
+    async refreshActiveUser () {
+     this.activeUser = await this.$auth.getUser()
+    },
+    async logout () {
+      await this.$auth.logout()
+      await this.refreshActiveUser()
+      this.$router.push('/loggedout')
+
+    }
+  },
   components: {
     // eslint-disable-next-line vue/no-unused-components
-    Modul
   }
   }
 
@@ -32,8 +67,14 @@ html.dark {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  margin-top: 65px
+}
+body {
+  background-color: #EAEDED;
+}
+.custom-contain {
+  margin-top: 100px;
 }
 #nav {
   padding: 30px;
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import * as Vue from 'vue'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView'
 import ProfileView from '../views/ProfileView'
 import ModuleView from '../views/ModuleView'
+import LoggedIn from '../components/LoggedIn'
+import LoggedOut from '../components/LoggedOut'
+import Auth from '@okta/okta-vue'
+
+Vue.use(Auth, {
+  issuer: 'https://dev-18548568.okta.com.com/oauth2/default',
+  clientId: '{0oa7sw1bo3gIxcIId5d7}',
+  redirectUri: 'http://localhost:3000/implicit/callback',
+  scope: 'openid profile email'
+})
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -24,6 +37,22 @@ const routes = [
     path: '/modules',
     name: 'modules',
     component: ModuleView
+  },   {
+    path: '/loggedout',
+    name: 'LoggedOut',
+    component: LoggedOut
+  },
+  {
+    path: '/implicit/callback',
+    component: Auth.handleCallback()
+  },
+  {
+    path:'/',
+    name: 'LoggedIn',
+    component: LoggedIn,
+    meta: {
+      requiresAuth: true
+    }
   }
 
 ]
@@ -34,4 +63,6 @@ const router = createRouter({
   routes
 })
 
-export default router
+router.beforeEach(Vue.prototype.$auth.authRedirectGuard())
+
+export default router()
